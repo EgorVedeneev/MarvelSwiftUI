a08ca4b4015a722cee0da2cb35f4c4f0b9b252dd
Egor <metal-ist@bk.ru>
2023-09-26 12:43:16 +0300

    Added Const class

---
+++ MarvelSwiftUI/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  MarvelSwiftUI
+//
+//  Created by egorvedeneev on 26.09.2023.
+//
+
+import Foundation
+
+class Const {
+    static let urlString = "https://gateway.marvel.com/v1/public/characters"
+    static let publicKey = "b55ff54838efe3a5a38e2b443b9880bd"
+    static let privateKey = "3a1cad95f793d7dfe4a56c7a0d13a132f56a1339"
+}
